d1e18ad5ce348c0549ecfe2e29e88bf597573fd9
incu6us <vyacheslavpryimak@gmail.com>
2020-05-09 16:49:21 +0300

    Brew formula update for goimports-reviser version v0.0.5

--- Formula/goimports-reviser.rb
+++ Formula/goimports-reviser.rb
@@ -7,16 +7,16 @@ class GoimportsReviser < Formula
 
   if OS.mac?
     url "http://github.com/incu6us/goimports-reviser/releases/download/v0.0.5/goimports-reviser_0.0.5_darwin_amd64.tar.gz"
-    sha256 "b4929cdf83714a15be7c1ec10308aa4d0a4e33499662e3a472ec4064a9139e2d"
+    sha256 "1dd8c1313febb1ce6274c7ec750b9e87d1e1fe041d75221b4d94d1aa359d49fc"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "http://github.com/incu6us/goimports-reviser/releases/download/v0.0.5/goimports-reviser_0.0.5_linux_amd64.tar.gz"
-      sha256 "a591868743abb60a1aa104590b65b66b6eddb30569f73204567af3dde8a4321c"
+      sha256 "5a47097c613b923b9cbae1787797a10554868708cba9af3abc5168a02366d607"
     end
   end
 
   def install
-    bin.install goimports-reviser
+    bin.install "goimports-reviser"
   end
 
   test do
