5e054205d40ad81dcc40a8c9399e686179404aa7
incu6us <vyacheslavpryimak@gmail.com>
2021-04-05 23:35:42 +0300

    Brew formula update for goimports-reviser version v2.3.5

--- Formula/goimports-reviser.rb
+++ Formula/goimports-reviser.rb
@@ -5,16 +5,16 @@
 class GoimportsReviser < Formula
   desc "Right imports sorting & code formatting tool"
   homepage "https://github.com/incu6us/goimports-reviser"
-  version "2.3.4"
+  version "2.3.5"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/incu6us/goimports-reviser/releases/download/v2.3.4/goimports-reviser_2.3.4_darwin_amd64.tar.gz"
-    sha256 "f114396faa6b49f7968c929f59b5f1a2c94fc81d4365428d79b19638046efcec"
+    url "https://github.com/incu6us/goimports-reviser/releases/download/v2.3.5/goimports-reviser_2.3.5_darwin_amd64.tar.gz"
+    sha256 "17509c6cb9a8d9e524b992fc5348bfdaa0ba449ff78465928ce67d9fab0ef7ec"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/incu6us/goimports-reviser/releases/download/v2.3.4/goimports-reviser_2.3.4_linux_amd64.tar.gz"
-    sha256 "29865cfb2d312ff453c655c9b9a37c6c4018143e0a7290220a54b3eb468e2743"
+    url "https://github.com/incu6us/goimports-reviser/releases/download/v2.3.5/goimports-reviser_2.3.5_linux_amd64.tar.gz"
+    sha256 "d8a246ba33161ba66cfca2f195a79c317604018c388f85f1b0b3885a15ae9bab"
   end
 
   def install
