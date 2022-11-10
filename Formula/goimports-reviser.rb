0c4d44c9fbf14e234f6bc8ba46fa1107387c53f2
incu6us <vyacheslavpryimak@gmail.com>
2022-11-10 22:43:56 +0200

    Brew formula update for goimports-reviser version v3.2.1

--- Formula/goimports-reviser.rb
+++ Formula/goimports-reviser.rb
@@ -5,20 +5,20 @@
 class GoimportsReviser < Formula
   desc "Right imports sorting & code formatting tool"
   homepage "https://github.com/incu6us/goimports-reviser"
-  version "3.2.0"
+  version "3.2.1"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/incu6us/goimports-reviser/releases/download/v3.2.0/goimports-reviser_3.2.0_darwin_arm64.tar.gz"
-      sha256 "cfd5d93e651064289fba8b440fce8368dc63b6ade5345e8f066865fdb66aa94f"
+    if Hardware::CPU.intel?
+      url "https://github.com/incu6us/goimports-reviser/releases/download/v3.2.1/goimports-reviser_3.2.1_darwin_amd64.tar.gz"
+      sha256 "a290e867b73901a8d386df5289ae071c5df5450c9ded055d94697ab3b3649e50"
 
       def install
         bin.install "goimports-reviser"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/incu6us/goimports-reviser/releases/download/v3.2.0/goimports-reviser_3.2.0_darwin_amd64.tar.gz"
-      sha256 "5b3891831b4300e2aab7e3d98b52ede38097b50cb10bcdd89343a1e13cf6716c"
+    if Hardware::CPU.arm?
+      url "https://github.com/incu6us/goimports-reviser/releases/download/v3.2.1/goimports-reviser_3.2.1_darwin_arm64.tar.gz"
+      sha256 "c91144a10e6fb49d48480dfb85f320b2c08045812042774c20945e38bee92123"
 
       def install
         bin.install "goimports-reviser"
@@ -28,8 +28,8 @@ class GoimportsReviser < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/incu6us/goimports-reviser/releases/download/v3.2.0/goimports-reviser_3.2.0_linux_amd64.tar.gz"
-      sha256 "a4501a2e7c2b461a26ccf8ee704e6912f8cb96723b9ee134103e5fbe406014f7"
+      url "https://github.com/incu6us/goimports-reviser/releases/download/v3.2.1/goimports-reviser_3.2.1_linux_amd64.tar.gz"
+      sha256 "d021b2e4f528ede49e8b99fc62100c6f7250de1b764a98461056e20d6f5ba098"
 
       def install
         bin.install "goimports-reviser"
